965ef121fb2911432a5f6784488e09d19bde1993
Demented <115271904+Demented-101@users.noreply.github.com>
2025-10-23 14:51:02 +0100

    Started Camera System
    - Added camera handler and anchor scripts
    - Added UI for camera controlling
    - pressing space will open camera UI

---
+++ Assets/Scripts/CameraAnchor.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class CameraAnchor : MonoBehaviour
+{
+    public float FOV = 60;
+}
---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,93 @@
+using NUnit.Framework;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CameraHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject inactiveCamAnchor;
+    [SerializeField] private GameObject[] camAnchors = new GameObject[10];
+    [SerializeField] private GameObject camHUD;
+    [SerializeField] private int startCam = 10;
+    [SerializeField] private float defaultFOV = 60;
+
+    public bool camActive { get; private set; } = false;
+    public int currentCam { get; private set; } = 1;
+
+    public UnityEvent<int> onCamChanged;
+    private Camera camComponent;
+
+    private void Start()
+    {
+        GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
+        camComponent = camera.GetComponent<Camera>();
+
+        currentCam = startCam;
+        SetCamerasActive(false);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SetCamerasActive(!camActive); // toggle camera open
+        }
+    }
+
+    public void SetCamerasActive(bool active)
+    {
+        camActive = active;
+        if (active) { MoveCamera(currentCam - 1); }
+        else { ReturnToDefault(); }
+
+        camHUD.SetActive(active);
+        camComponent.fieldOfView = defaultFOV;
+    }
+
+    public void SetCamera(int cam)
+    {
+        if (cam < 1 || cam > 10) return; // cam doesn't exist
+
+        currentCam = cam;
+        onCamChanged?.Invoke(cam);
+        MoveCamera(cam - 1);
+        UpdateCameraSettings(cam - 1);
+
+        Debug.Log("hauii");
+    }
+
+    private void ReturnToDefault()
+    {
+        MoveCamera(-1);
+        UpdateCameraSettings(-1);
+    }
+
+    private void MoveCamera(int target)
+    {
+        GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
+        Transform targetTrans = inactiveCamAnchor.transform;
+        if (target != -1) { targetTrans = camAnchors[target].transform; }
+
+        // copy transform position and rotation to camera
+        camera.transform.position = targetTrans.position;
+        camera.transform.rotation = targetTrans.rotation;
+    }
+
+    private void UpdateCameraSettings(int target)
+    {
+        if (target == -1)
+        {
+            camComponent.fieldOfView = defaultFOV;
+            return;
+        }
+
+        CameraAnchor camAnchor = camAnchors[target].GetComponent<CameraAnchor>();
+        if (camAnchor != null)
+        {
+            camComponent.fieldOfView = camAnchor.FOV;
+        }
+        else
+        {
+            UpdateCameraSettings(-1);
+        }
+    }
+}
